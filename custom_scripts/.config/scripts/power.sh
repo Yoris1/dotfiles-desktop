7811bd3f44724cd95c9335aba18bcd2c776c3b81
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2021-05-10 23:42:16 +0300

    Add powermenu and autolock

--- custom_scripts/.config/scripts/power.sh
+++ custom_scripts/.config/scripts/power.sh
@@ -1,1 +1,14 @@
-echo -e "Poweroff\nReboot\nExit" | rofi -dmenu -p "Action" -config ~/.config/rofi/powermenu.rasi
+res=`echo -e "Suspend\nShutdown\nReboot\nExit" | rofi -dmenu -p "Action" -config ~/.config/rofi/powermenu.rasi`
+if [[ $res = "Suspend" ]]
+then
+	echo Suspending
+	systemctl suspend
+elif [[ $res = "Shutdown" ]]
+	then
+	echo Powering Off
+	poweroff
+elif [[ $res = "Reboot" ]]
+	then
+	echo Rebooting
+	reboot
+fi
