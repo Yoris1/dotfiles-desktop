120bfa9e7815b7b07911a42fe08decc0917792bb
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2021-07-26 20:29:08 +0300

    add support for pywalfox

--- custom_scripts/.config/scripts/theme.sh
+++ custom_scripts/.config/scripts/theme.sh
@@ -9,4 +9,5 @@ ${HOME}/.config/scripts/dunst/retheme_dunst.sh
 convert $1 -resize 256x256 -gravity center -crop 144x144+0+0 ${HOME}/.cache/rice_icon.png
 notify-send "New theme!" $1 --icon ${HOME}/.cache/rice_icon.png
 source ~/.cache/wal/colors.sh
+python -m pywalfox update
 echo -e "@import \"~/.cache/wal/colors-rofi-dark\" \n* { background: ${color0}7b; }" > ~/.cache/rofi_colors.rasi
