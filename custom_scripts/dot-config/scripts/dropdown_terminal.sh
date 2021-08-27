1cbf617100d756aa3f317304c43cd5ac9231ee80
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2021-08-27 19:07:26 +0300

    debloat dropdown_terminal

--- custom_scripts/dot-config/scripts/dropdown_terminal.sh
+++ custom_scripts/dot-config/scripts/dropdown_terminal.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
 KITTY_ID=`xdotool search --name "scratch"`
 if [[ -z "$KITTY_ID" ]] 
 then
 	kitty --title scratch &
-	sleep 0.5
 else
 	bspc node $KITTY_ID --flag hidden
 	bspc node -f $KITTY_ID
