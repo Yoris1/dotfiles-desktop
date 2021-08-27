20261a59bb0a54efdf1f3cc88e20fffb750dcec1
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2021-08-27 19:04:58 +0300

    make creating scratch window look nicer

--- custom_scripts/dot-config/scripts/dropdown_terminal.sh
+++ custom_scripts/dot-config/scripts/dropdown_terminal.sh
@@ -3,8 +3,7 @@ if [[ -z "$KITTY_ID" ]]
 then
 	kitty --title scratch &
 	sleep 0.5
+else
+	bspc node $KITTY_ID --flag hidden
+	bspc node -f $KITTY_ID
 fi
-KITTY_ID=`xdotool search --name "scratch"`
-bspc node $KITTY_ID --flag hidden
-bspc node -f $KITTY_ID
-echo $KITTY_ID
