cd13680b80dff5d03a6b83b4b90e0e07ace5f1c4
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2021-10-24 13:42:00 +0300

    create 10 workspaces per monitor

--- custom_scripts/dot-config/scripts/start_polybar.sh
+++ custom_scripts/dot-config/scripts/start_polybar.sh
@@ -1,4 +1,4 @@
 source ~/.cache/wal/colors.sh
 export bg="#b0${color1/'#'}"
 echo $bg
-(polybar -r workspaces & polybar -r title & polybar -r tray)
+(polybar -r workspaces & polybar -r title & polybar -r tray & polybar -r workspaces-secondary)
