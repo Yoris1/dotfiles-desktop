a232098dd39752acfac66bf6462c3c4685db7b7e
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2021-10-24 21:23:23 +0300

    replace main bar

--- custom_scripts/dot-config/scripts/start_polybar.sh
+++ custom_scripts/dot-config/scripts/start_polybar.sh
@@ -1,4 +1,4 @@
 source ~/.cache/wal/colors.sh
 export bg="#b0${color1/'#'}"
 echo $bg
-(polybar -r workspaces & polybar -r title & polybar -r tray & polybar -r workspaces-secondary)
+(polybar -r main & polybar -r workspaces-secondary)
