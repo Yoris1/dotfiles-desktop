74cf4345376b7d39e8f3181b5854cb0d53c85e23
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2021-10-26 17:56:00 +0300

    add ability to get media title and playing status

--- custom_scripts/dot-config/scripts/music/media_info.sh
+++ custom_scripts/dot-config/scripts/music/media_info.sh
@@ -1,3 +1,11 @@
+function getMpdStatus() {
+	status=`mpc --format ""` # --format "" removes song name in case it has "paused" or "playing" in it 
+	if [[ $status == *"playing"* ]]; then
+		echo Playing
+	elif [[ $status == *"paused"* ]]; then
+		echo Paused
+	fi
+}
 function getPlayer() {
 	players=`playerctl -l`
 	for i in $players; do
@@ -5,16 +13,15 @@ function getPlayer() {
 		if [[ $status = $1 ]] # $1 can either be "Playing" or "Paused" 
 		then
 			echo $i 
-			break
+			exit 0
 		fi
 	done
-	mpd_status=`mpc --format ""` # --format "" removes song name in case it has "paused" or "playing" in it 
-	if [[ $mpd_status == *"${1,,}"* ]]; then 
+	if [[ `getMpdStatus` == $1 ]]; then 
 		echo mpd
 	fi
 }
 function sendCommandToPlayer() {
-	# $1 is player name, $2 is play/pause/toggle/stop/next/prev
+	# $1 is player name, $2 is play/pause/toggle/stop/next/prev/status
 	backend="mpc"
 	command="$2"
 	if [[ $1 != "mpd" ]]; then
@@ -27,22 +34,31 @@ function sendCommandToPlayer() {
 				command="previous"
 			;;
 		esac
+	elif [[ $2 == "status" ]]; then 
+		# asking for mpd status; edge case cause mpc has no one command for status
+		echo `getMpdStatus`
+		exit 0
 	fi
 	$backend $command # send $command to player with the $backend backend
 }
-function getPlayerStatus() {
-	# $1 is a playerctl player id or mpd
-	pass
+
+function getPlayerTitle() {
+	# $1 is a playerctl player id or "mpd"; output nicely formated song title
+	if [[ $1 == "mpd" ]]; then
+		echo `mpc current -f "%file%"`
+	else 
+		echo `playerctl -p $1 metadata title`
+	fi
 }
+
 
 if [[ $# -eq 1 ]]; then
 	getPlayer $1 # $1 is "Playing" or "Paused"
 elif [[ $# -eq 2 ]]; then
-	# $1 is player name; $2 is play/pause/toggle/stop/next/prev/status
-	# TODO: add a title command
-	if [[ $2 != status ]]; then
+	# $1 is player name; $2 is play/pause/toggle/stop/next/prev/status/title
+	if [[ $2 != "title" ]]; then
 		sendCommandToPlayer $1 $2
-	else 
-		echo `getPlayerStatus $1`
+	else
+		echo `getPlayerTitle $1`
 	fi
 fi
