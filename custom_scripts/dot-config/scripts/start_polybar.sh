808fa7e1374322ebaf1c98bbcef4e4330e58f584
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2022-05-07 14:49:31 +0300

    fix up start polybar script

--- custom_scripts/dot-config/scripts/start_polybar.sh
+++ custom_scripts/dot-config/scripts/start_polybar.sh
@@ -1,4 +1,12 @@
 source ~/.cache/wal/colors.sh
 export bg="#b0${color1/'#'}"
 echo $bg
-(polybar -r main & polybar -r workspaces-secondary)
+bars=`ps aux | grep polybar`
+if [[ $bars != *"main"* ]]
+then
+	polybar -r main &
+fi
+if [[ $bars != *"workspaces-secondary"* ]] 
+then
+	polybar -r workspaces-secondary &
+fi
