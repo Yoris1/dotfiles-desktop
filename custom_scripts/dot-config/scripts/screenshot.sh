ea87b31a50808538d6f4aea7cd6e4d58cbc71519
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2022-05-07 14:48:05 +0300

    fix screenshotting with newer scrot

--- custom_scripts/dot-config/scripts/screenshot.sh
+++ custom_scripts/dot-config/scripts/screenshot.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/pics/screenshots
 FILENAME=~/pics/screenshots/`date -Iseconds`.png
 if [ $1 ]; then
-	scrot -zfsp $FILENAME && gimp --no-data $FILENAME 
+	scrot -zfs $FILENAME && gimp --no-data $FILENAME 
 else
-	scrot -zfsp $FILENAME
+	scrot -zfs $FILENAME
 	xclip -selection clipboard -t image/png -i $FILENAME
 fi
 
