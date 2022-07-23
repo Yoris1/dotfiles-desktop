5df96b9ff3fb2f3977f79571a322b4f697cdb125
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2022-07-23 22:53:30 +0300

    delete old useless script

--- custom_scripts/dot-config/scripts/cycle_theme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FILE=${HOME}/.cache/theme_a
-if [ -f "$FILE" ] ; then
-	rm "$FILE"
-	${HOME}/.config/scripts/theme.sh ${HOME}/.config/wallpapers/a.png
-else
-	touch "$FILE"
-	${HOME}/.config/scripts/theme.sh ${HOME}/.config/wallpapers/b.png
-fi
