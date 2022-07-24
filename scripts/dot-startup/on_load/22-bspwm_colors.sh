772fe3f5dde4182adbd310a2ac3ffe3049491cde
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2022-07-24 08:07:43 +0300

    separate bspwm colors

---
+++ scripts/dot-startup/on_load/22-bspwm_colors.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source "${HOME}/.cache/wal/colors.sh"
+bspc config normal_border_color		"$color4"
+bspc config presel_feedback_color	"$color2"
+bspc config focused_border_color	"$color1"
