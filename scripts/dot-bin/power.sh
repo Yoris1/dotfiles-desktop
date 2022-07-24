4c9eb50483f54c71972966ac8123e2e5185a8b3a
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2022-07-24 08:57:28 +0300

    upgrade power script

--- scripts/dot-bin/power.sh
+++ scripts/dot-bin/power.sh
@@ -1,8 +1,10 @@
-res=`echo -e "Suspend\nShutdown\nReboot\nExit" | rofi -dmenu -p "Action" -config ~/.config/rofi/powermenu.rasi`
+res=`echo -e "Suspend\nLock\nShutdown\nReboot\nExit" | rofi -dmenu -i -p "Action" -config ~/.config/rofi/powermenu.rasi`
 if [[ $res = "Suspend" ]] ; then
 	systemctl suspend
 elif [[ $res = "Shutdown" ]] ; then
 	poweroff
 elif [[ $res = "Reboot" ]] ; then
 	reboot
+elif [[ $res = "Lock" ]] ; then
+	~/.bin/lock.sh
 fi
