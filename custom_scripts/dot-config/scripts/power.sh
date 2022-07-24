2e5f1c69fa589f8a4febb1f7fb3fcdbb984ec9fc
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2022-07-24 08:43:24 +0300

    cleanup power script

--- custom_scripts/dot-config/scripts/power.sh
+++ custom_scripts/dot-config/scripts/power.sh
@@ -1,14 +1,8 @@
 res=`echo -e "Suspend\nShutdown\nReboot\nExit" | rofi -dmenu -p "Action" -config ~/.config/rofi/powermenu.rasi`
-if [[ $res = "Suspend" ]]
-then
-	echo Suspending
+if [[ $res = "Suspend" ]] ; then
 	systemctl suspend
-elif [[ $res = "Shutdown" ]]
-	then
-	echo Powering Off
+elif [[ $res = "Shutdown" ]] ; then
 	poweroff
-elif [[ $res = "Reboot" ]]
-	then
-	echo Rebooting
+elif [[ $res = "Reboot" ]] ; then
 	reboot
 fi
