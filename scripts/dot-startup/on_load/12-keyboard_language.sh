5b9a8905e24c18dc722fc48f51e2b244fa829bdc
Yoris1 <41749115+Yoris1@users.noreply.github.com>
2022-07-24 08:05:50 +0300

    minify xinitrc

--- scripts/dot-startup/on_load/12-keyboard_language.sh
+++ scripts/dot-startup/on_load/12-keyboard_language.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-setxkbmap us,lt -option grp:caps_toggle
-(sleep 1 && xmodmap ${HOME}/.Xmodmap) &
+usermodmap=$HOME/.Xmodmap
+[ -f $usermodmap ] && xmodmap $usermodmap || setxkbmap us,lt -option grp:caps_toggle
